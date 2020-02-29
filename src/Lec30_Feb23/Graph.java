a534a25cf7b0e768592408eaac51d574739d723f
frankenstein32 <saifirashid32@gmail.com>
2020-02-29 13:09:36 +0530

    Lec 30: 29/02/2020

--- src/Lec30_Feb23/Graph.java
+++ src/Lec30_Feb23/Graph.java
@@ -1,7 +1,9 @@
 package Lec30_Feb23;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Graph {
@@ -136,10 +138,17 @@ public class Graph {
 	class pair {
 		int data;
 		String psf;
+		String color;
 
 		public pair(int d, String p) {
 			data = d;
 			psf = p;
+		}
+
+		public pair(int d, String p, String c) {
+			data = d;
+			psf = p;
+			color = c;
 		}
 	}
 
@@ -176,6 +185,46 @@ public class Graph {
 				if (containsEdge(rp.data, nbr) && !visited.containsKey(nbr)) {
 					pair nbrPair = new pair(nbr, rp.psf + rp.data);
 					queue.addLast(nbrPair);
+				}
+			}
+
+		}
+
+		return false;
+
+	}
+
+	public boolean DFS(int u, int v) {
+
+		LinkedList<pair> stack = new LinkedList<>();
+
+		pair np = new pair(u, "");
+		stack.addFirst(np);
+
+		HashMap<Integer, Boolean> visited = new HashMap<>();
+
+		while (!stack.isEmpty()) {
+
+			pair rp = stack.removeFirst();
+
+			if (visited.containsKey(rp.data)) {
+
+				// isCyclic
+				continue;
+			}
+
+			if (rp.data == v) {
+				System.out.println(rp.psf + v);
+				return true;
+			}
+
+			visited.put(rp.data, true);
+
+			for (int nbr = 1; nbr <= numV; nbr++) {
+
+				if (containsEdge(rp.data, nbr) && !visited.containsKey(nbr)) {
+					pair nbrPair = new pair(nbr, rp.psf + rp.data);
+					stack.addFirst(nbrPair);
 				}
 			}
 
@@ -224,6 +273,402 @@ public class Graph {
 			}
 
 		}
+	}
+
+	public void DFT() {
+
+		HashMap<Integer, Boolean> visited = new HashMap<>();
+
+		for (int u = 1; u <= numV; u++) {
+
+			if (visited.containsKey(u)) {
+				continue;
+			}
+
+			LinkedList<pair> stack = new LinkedList<>();
+			pair np = new pair(u, "");
+			stack.addFirst(np);
+
+			while (!stack.isEmpty()) {
+
+				pair rp = stack.removeFirst();
+
+				if (visited.containsKey(rp.data)) {
+
+					// isCyclic
+					continue;
+				}
+
+				System.out.println(rp.data + " -> " + rp.psf + rp.data + ", ");
+
+				visited.put(rp.data, true);
+
+				for (int nbr = 1; nbr <= numV; nbr++) {
+
+					if (containsEdge(rp.data, nbr) && !visited.containsKey(nbr)) {
+						pair nbrPair = new pair(nbr, rp.psf + rp.data);
+						stack.addFirst(nbrPair);
+					}
+				}
+
+			}
+
+		}
+	}
+
+	public boolean isCyclic() {
+
+		HashMap<Integer, Boolean> visited = new HashMap<>();
+
+		for (int u = 1; u <= numV; u++) {
+
+			if (visited.containsKey(u)) {
+				continue;
+			}
+
+			LinkedList<pair> queue = new LinkedList<>();
+			pair np = new pair(u, "");
+			queue.addLast(np);
+
+			while (!queue.isEmpty()) {
+
+				pair rp = queue.removeFirst();
+
+				if (visited.containsKey(rp.data)) {
+
+					return true;
+				}
+
+				visited.put(rp.data, true);
+
+				for (int nbr = 1; nbr <= numV; nbr++) {
+
+					if (containsEdge(rp.data, nbr) && !visited.containsKey(nbr)) {
+						pair nbrPair = new pair(nbr, rp.psf + rp.data);
+						queue.addLast(nbrPair);
+					}
+				}
+
+			}
+
+		}
+
+		return false;
+	}
+
+	public boolean isConnected() {
+
+		HashMap<Integer, Boolean> visited = new HashMap<>();
+
+		int cc = 0;
+		for (int u = 1; u <= numV; u++) {
+
+			if (visited.containsKey(u)) {
+				continue;
+			}
+
+			cc++;
+
+			LinkedList<pair> queue = new LinkedList<>();
+			pair np = new pair(u, "");
+			queue.addLast(np);
+
+			while (!queue.isEmpty()) {
+
+				pair rp = queue.removeFirst();
+
+				if (visited.containsKey(rp.data)) {
+
+					// isCyclic
+					continue;
+				}
+
+				visited.put(rp.data, true);
+
+				for (int nbr = 1; nbr <= numV; nbr++) {
+
+					if (containsEdge(rp.data, nbr) && !visited.containsKey(nbr)) {
+						pair nbrPair = new pair(nbr, rp.psf + rp.data);
+						queue.addLast(nbrPair);
+					}
+				}
+
+			}
+
+		}
+
+		return cc == 1;
+	}
+
+	public boolean isTree() {
+
+		return isConnected() && !isCyclic();
+	}
+
+	public ArrayList<ArrayList<Integer>> getConnectedComp() {
+
+		HashMap<Integer, Boolean> visited = new HashMap<>();
+		ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+
+		for (int u = 1; u <= numV; u++) {
+
+			if (visited.containsKey(u)) {
+				continue;
+			}
+
+			ArrayList<Integer> eachLevel = new ArrayList<>();
+
+			LinkedList<pair> queue = new LinkedList<>();
+			pair np = new pair(u, "");
+			queue.addLast(np);
+
+			while (!queue.isEmpty()) {
+
+				pair rp = queue.removeFirst();
+
+				if (visited.containsKey(rp.data)) {
+
+					// isCyclic
+					continue;
+				}
+
+				eachLevel.add(rp.data);
+
+				visited.put(rp.data, true);
+
+				for (int nbr = 1; nbr <= numV; nbr++) {
+
+					if (containsEdge(rp.data, nbr) && !visited.containsKey(nbr)) {
+						pair nbrPair = new pair(nbr, rp.psf + rp.data);
+						queue.addLast(nbrPair);
+					}
+				}
+
+			}
+
+			ans.add(eachLevel);
+
+		}
+
+		return ans;
+	}
+
+	public boolean isBipertite() {
+
+		HashMap<Integer, String> visited = new HashMap<>();
+
+		for (int u = 1; u <= numV; u++) {
+
+			if (visited.containsKey(u)) {
+				continue;
+			}
+
+			LinkedList<pair> queue = new LinkedList<>();
+			pair np = new pair(u, "", "R");
+			queue.addLast(np);
+
+			while (!queue.isEmpty()) {
+
+				pair rp = queue.removeFirst();
+
+				if (visited.containsKey(rp.data)) {
+
+					String oc = visited.get(rp.data);
+					String nc = rp.color;
+
+					if (!oc.equals(nc)) {
+						return false;
+					}
+
+					continue;
+				}
+
+				visited.put(rp.data, rp.color);
+
+				for (int nbr = 1; nbr <= numV; nbr++) {
+
+					if (containsEdge(rp.data, nbr) && !visited.containsKey(nbr)) {
+
+						String c = rp.color.equals("R") ? "G" : "R";
+						pair nbrPair = new pair(nbr, rp.psf + rp.data, c);
+						queue.addLast(nbrPair);
+					}
+				}
+
+			}
+
+		}
+
+		return true;
+	}
+
+	class PrimsPair implements Comparable<PrimsPair> {
+
+		int v;
+		int c;
+
+		PrimsPair(int v, int c) {
+			this.v = v;
+			this.c = c;
+		}
+
+		@Override
+		public int compareTo(PrimsPair o) {
+			return this.c - o.c;
+		}
+	}
+
+	class resultPair {
+
+		int parent;
+		int wt;
+
+		public resultPair() {
+			// TODO Auto-generated constructor stub
+		}
+
+		resultPair(int p, int w) {
+			parent = p;
+			wt = w;
+		}
+	}
+
+	public void MST() {
+
+		boolean[] isMst = new boolean[numV + 1];
+		int[] cost = new int[numV + 1];
+
+		resultPair[] resultSet = new resultPair[numV + 1];
+
+		for (int i = 1; i <= numV; i++) {
+			cost[i] = Integer.MAX_VALUE;
+			resultSet[i] = new resultPair();
+		}
+
+		PriorityQueue<PrimsPair> pq = new PriorityQueue<>();
+
+		// cost
+		cost[1] = 0;
+
+		// PrimsPair
+		PrimsPair np = new PrimsPair(1, 0);
+
+		// ResultPair
+		resultSet[1].parent = -1;
+		resultSet[1].wt = 0;
+
+		pq.add(np);
+
+		while (!pq.isEmpty()) {
+
+			PrimsPair rp = pq.remove();
+
+			if (isMst[rp.v]) {
+				continue;
+			}
+
+			isMst[rp.v] = true;
+
+			for (int nbr = 1; nbr <= numV; nbr++) {
+
+				if (containsEdge(nbr, rp.v) && !isMst[nbr]) {
+
+					int oc = cost[nbr];
+					int nc = matrix[rp.v][nbr];
+
+					if (nc < oc) {
+
+						cost[nbr] = nc;
+						PrimsPair temp = new PrimsPair(nbr, nc);
+						pq.add(temp);
+
+						resultSet[nbr].parent = rp.v;
+						resultSet[nbr].wt = nc;
+
+					}
+
+				}
+
+			}
+
+		}
+
+		printMST(resultSet);
+
+	}
+
+	public void printMST(resultPair[] set) {
+
+		for (int i = 1; i < set.length; i++) {
+
+			System.out.println(set[i].parent + " - " + i + " : " + set[i].wt);
+
+		}
+
+	}
+
+	public void Dijkstra() {
+
+		boolean[] isMst = new boolean[numV + 1];
+		int[] cost = new int[numV + 1];
+
+		resultPair[] resultSet = new resultPair[numV + 1];
+
+		for (int i = 1; i <= numV; i++) {
+			cost[i] = Integer.MAX_VALUE;
+			resultSet[i] = new resultPair();
+		}
+
+		PriorityQueue<PrimsPair> pq = new PriorityQueue<>();
+
+		// cost
+		cost[1] = 0;
+
+		// PrimsPair
+		PrimsPair np = new PrimsPair(1, 0);
+
+		// ResultPair
+		resultSet[1].parent = -1;
+		resultSet[1].wt = 0;
+
+		pq.add(np);
+
+		while (!pq.isEmpty()) {
+
+			PrimsPair rp = pq.remove();
+
+			if (isMst[rp.v]) {
+				continue;
+			}
+
+			isMst[rp.v] = true;
+
+			for (int nbr = 1; nbr <= numV; nbr++) {
+
+				if (containsEdge(nbr, rp.v) && !isMst[nbr]) {
+
+					int oc = cost[nbr];
+					int nc = matrix[rp.v][nbr] + cost[rp.v];
+
+					if (nc < oc) {
+
+						cost[nbr] = nc;
+						PrimsPair temp = new PrimsPair(nbr, nc);
+						pq.add(temp);
+
+						resultSet[nbr].parent = rp.v;
+						resultSet[nbr].wt = nc;
+
+					}
+
+				}
+
+			}
+
+		}
+
+		printMST(resultSet);
+
 	}
 
 }
--- src/Lec30_Feb23/GraphClient.java
+++ src/Lec30_Feb23/GraphClient.java
@@ -5,35 +5,45 @@ public class GraphClient {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Graph gp = new Graph(7);
+		Graph gp = new Graph(5);
 		gp.addEdge(1, 2, 3);
-		gp.addEdge(1, 4, 2);
-		gp.addEdge(2, 3, 4);
-		gp.addEdge(2, 5, 5);
-		gp.addEdge(3, 4, 1);
-		gp.addEdge(5, 6, 6);
-		gp.addEdge(5, 7, 1);
-		gp.addEdge(6, 7, 2);
+		gp.addEdge(1, 3, 2);
+		gp.addEdge(2, 4, 1);
+		gp.addEdge(3, 4, 5);
+		gp.addEdge(2, 5, 6);
+		gp.addEdge(4, 5, 7);
+//		gp.addEdge(5, 7, 1);
+//		gp.addEdge(6, 7, 2);
 
 		System.out.println(gp);
 
 		System.out.println(gp.numEdges());
-		System.out.println(gp.removeEdge(2, 5));
-		System.out.println(gp.removeEdge(5, 6));
-		System.out.println(gp.removeEdge(7, 6));
+//		System.out.println(gp.removeEdge(2, 5));
+//		System.out.println(gp.removeEdge(5, 6));
+//		System.out.println(gp.removeEdge(7, 6));
 
 		System.out.println(gp);
-
-		System.out.println(gp.hasPath(1, 7));
-		System.out.println("-------------");
-
-		gp.hasAllPath(1, 7);
-
-		System.out.println("-------------");
-
-		System.out.println(gp.BFS(1, 7));
-
-		gp.BFT();
+//
+//		System.out.println(gp.hasPath(1, 5));
+//		System.out.println("-------------");
+//
+//		gp.hasAllPath(1, 5);
+//
+//		System.out.println("-------------");
+//
+//		System.out.println(gp.BFS(1, 7));
+//		System.out.println("---------");
+//		System.out.println(gp.DFS(1, 7));
+//
+//		gp.DFT();
+//		System.out.println(gp.isCyclic());
+//		System.out.println(gp.isConnected());
+//		System.out.println(gp.isTree());
+//
+//		System.out.println(gp.getConnectedComp());
+		gp.MST();
+		System.out.println("---------");
+		gp.Dijkstra();
 
 	}
 
